4ca4ede409e0cf533290386f6390e2123509d963
AMereBagatelle <themerebagatelle@gmail.com>
2020-09-11 20:28:01 -0400

    Set up MonoColorSkybox

--- src/main/java/amerebagatelle/github/io/fabricskyboxes/mixin/WorldRendererAccess.java
+++ src/main/java/amerebagatelle/github/io/fabricskyboxes/mixin/WorldRendererAccess.java
@@ -1,7 +1,10 @@
 package amerebagatelle.github.io.fabricskyboxes.mixin;
 
+import net.minecraft.client.gl.VertexBuffer;
+import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.texture.TextureManager;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -10,4 +13,21 @@ public interface WorldRendererAccess {
     @Accessor
     TextureManager getTextureManager();
 
+    @Accessor
+    VertexFormat getSkyVertexFormat();
+
+    @Accessor
+    VertexBuffer getLightSkyBuffer();
+
+    @Accessor
+    VertexBuffer getStarsBuffer();
+
+    @Accessor
+    VertexBuffer getDarkSkyBuffer();
+
+    @Accessor
+    Identifier getSUN();
+
+    @Accessor
+    Identifier getMOON_PHASES();
 }
