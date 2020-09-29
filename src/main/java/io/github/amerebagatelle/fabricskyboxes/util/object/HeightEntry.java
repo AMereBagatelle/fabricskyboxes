32c8d6a8254a4817640e4bf99ddd970699af89d4
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-29 20:52:35 +0530

    Object holders
     On branch 1.16.x-dev
     Changes to be committed:
    	modified:   src/main/java/io/github/amerebagatelle/fabricskyboxes/skyboxes/MonoColorSkybox.java
    	new file:   src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Fade.java
    	new file:   src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/HeightEntry.java
    	new file:   src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/RGBA.java
    	new file:   src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Textures.java
    	new file:   src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Weather.java

---
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/HeightEntry.java
@@ -0,0 +1,39 @@
+package io.github.amerebagatelle.fabricskyboxes.util.object;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public class HeightEntry {
+    public static final Codec<HeightEntry> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.FLOAT.fieldOf("min").forGetter(HeightEntry::getMin),
+            Codec.FLOAT.fieldOf("max").forGetter(HeightEntry::getMax)
+    ).apply(instance, HeightEntry::new));
+    private final float min;
+    private final float max;
+
+    public HeightEntry(float min, float max) {
+        if (min > max) {
+            throw new IllegalStateException("Maximum value is lower than the minimum value:\n" + this.toString());
+        }
+        this.min = min;
+        this.max = max;
+    }
+
+    public float getMin() {
+        return this.min;
+    }
+
+    public float getMax() {
+        return this.max;
+    }
+
+    public float[] toFloatArray() {
+        return new float[]{this.min, this.max};
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
---
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Weather.java
@@ -0,0 +1,39 @@
+package io.github.amerebagatelle.fabricskyboxes.util.object;
+
+import java.util.Map;
+import java.util.Objects;
+
+import com.google.common.collect.ImmutableMap;
+import com.mojang.serialization.Codec;
+
+public enum Weather {
+    CLEAR("clear"),
+    RAIN("rain"),
+    SNOW("snow"),
+    THUNDER("thunder");
+
+    public static final Codec<Weather> CODEC = Codec.STRING.xmap(Weather::fromString, Weather::toString);
+    private static final Map<String, Weather> VALUES;
+    private final String name;
+
+    Weather(String name) {
+        this.name = name;
+    }
+
+    public static Weather fromString(String name) {
+        return Objects.requireNonNull(VALUES.get(name));
+    }
+
+    static {
+        ImmutableMap.Builder<String, Weather> builder = ImmutableMap.builder();
+        for (Weather value : values()) {
+            builder.put(value.name, value);
+        }
+        VALUES = builder.build();
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
