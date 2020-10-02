e8b361db9c3b92614cc3f6fd244c5a6c456f2ebb
AMereBagatelle <themerebagatelle@gmail.com>
2020-10-02 10:18:17 -0400

    Remove accidentally merged DevelopmentTests development environment check
    Signed-off-by: AMereBagatelle <themerebagatelle@gmail.com>

--- src/testmod/java/io/github/amerebagatelle/fabricskyboxestest/DevelopmentTests.java
+++ src/testmod/java/io/github/amerebagatelle/fabricskyboxestest/DevelopmentTests.java
@@ -1,10 +1,11 @@
 package io.github.amerebagatelle.fabricskyboxestest;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.mojang.serialization.JsonOps;
 import io.github.amerebagatelle.fabricskyboxes.FabricSkyBoxesClient;
 import io.github.amerebagatelle.fabricskyboxes.skyboxes.MonoColorSkybox;
 import io.github.amerebagatelle.fabricskyboxes.skyboxes.textured.SquareTexturedSkybox;
@@ -12,24 +13,18 @@ import io.github.amerebagatelle.fabricskyboxes.util.object.DecorationTextures;
 import io.github.amerebagatelle.fabricskyboxes.util.object.Fade;
 import io.github.amerebagatelle.fabricskyboxes.util.object.RGBA;
 import io.github.amerebagatelle.fabricskyboxes.util.object.Textures;
-import com.google.common.collect.Lists;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.mojang.serialization.JsonOps;
-
-import net.minecraft.screen.PlayerScreenHandler;
-
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.screen.PlayerScreenHandler;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DevelopmentTests implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
-        if (!FabricLoader.getInstance().isDevelopmentEnvironment() || System.getProperty("fabricskyboxes.runTests") == null || !System.getProperty("fabricskyboxes.runTests").equals("true")) {
-            return;
-        }
         FabricSkyBoxesClient.getLogger().info("Testing FabricSkyboxes skybox creation. ");
         try {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
