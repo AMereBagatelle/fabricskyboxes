2ced8e62f9a6cd5f867860ac5b139aeaa6efc9df
AMereBagatelle <themerebagatelle@gmail.com>
2020-10-27 17:04:55 -0400

    Fix indentation
    Signed-off-by: AMereBagatelle <themerebagatelle@gmail.com>

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Rotation.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Rotation.java
@@ -4,16 +4,15 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-
 import net.minecraft.client.util.math.Vector3f;
 
 public class Rotation {
     private static final Codec<Vector3f> VEC_3_F = Codec.FLOAT.listOf().comapFlatMap((list) -> {
-            if (list.size() < 3) {
-                return DataResult.error("Incomplete number of elements in vector");
-            }
-            return DataResult.success(new Vector3f(list.get(0), list.get(1), list.get(2)));
-        }, (vec) -> ImmutableList.of(vec.getX(), vec.getY(), vec.getZ()));
+        if (list.size() < 3) {
+            return DataResult.error("Incomplete number of elements in vector");
+        }
+        return DataResult.success(new Vector3f(list.get(0), list.get(1), list.get(2)));
+    }, (vec) -> ImmutableList.of(vec.getX(), vec.getY(), vec.getZ()));
     public static final Codec<Rotation> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             VEC_3_F.fieldOf("static").forGetter(Rotation::getStatic),
             VEC_3_F.optionalFieldOf("axis", new Vector3f(0F, 0F, 0F)).forGetter(Rotation::getAxis)
