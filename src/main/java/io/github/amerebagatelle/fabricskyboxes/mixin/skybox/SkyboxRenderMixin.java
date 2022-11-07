88d043ec341eca0689299a7cf4b9a9dc42cb5370
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-07 17:21:14 +0800

    Add FSB toggle

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/mixin/skybox/SkyboxRenderMixin.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/mixin/skybox/SkyboxRenderMixin.java
@@ -1,29 +1,27 @@
 package io.github.amerebagatelle.fabricskyboxes.mixin.skybox;
 
 import io.github.amerebagatelle.fabricskyboxes.SkyboxManager;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class SkyboxRenderMixin {
-    @Shadow @Final private MinecraftClient client;
-
     /**
      * Contains the logic for when skyboxes should be rendered.
      */
     @Inject(method = "renderSky", at = @At("HEAD"), cancellable = true)
     private void renderCustomSkyboxes(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-        float total = SkyboxManager.getInstance().getTotalAlpha();
-        SkyboxManager.getInstance().renderSkyboxes((WorldRendererAccess) this, matrices, tickDelta);
-        if (total > SkyboxManager.MINIMUM_ALPHA) {
-            ci.cancel();
+        SkyboxManager skyboxManager = SkyboxManager.getInstance();
+        if (skyboxManager.isEnabled()) {
+            float total = skyboxManager.getTotalAlpha();
+            skyboxManager.renderSkyboxes((WorldRendererAccess) this, matrices, tickDelta);
+            if (total > SkyboxManager.MINIMUM_ALPHA) {
+                ci.cancel();
+            }
         }
     }
 }
