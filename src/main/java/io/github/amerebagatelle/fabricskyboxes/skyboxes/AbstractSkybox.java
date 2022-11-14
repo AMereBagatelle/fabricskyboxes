bc3f8736a7a6ff0cb5934648f56167feace7a356
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-11-15 03:52:52 +0800

    Add loop condition (#69)
    * Add loop condition
    
    This commit adds new feature that allows skyboxes to be rendered in specific days in a loop of days, this is similar to OptiFine's `days` and `daysLoop` feature.
    
    * Clamp days to positive
    
    * Assert days are greater than 0
    
    * Add missing example
    
    * Use closed ranges and floating point `currentDay` for loop
    
    * Change loop days into floating point
    
    * Make loop day start at fade in time
    
    * Normalize negative `currentTime`
    
    euclidean modulo isn't gonna work here
    
    * Fix typo
    
    * Format JSONs and add priority field in full example
    
    * Clean up documentation

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/skyboxes/AbstractSkybox.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/skyboxes/AbstractSkybox.java
@@ -54,6 +54,7 @@ public abstract class AbstractSkybox {
     protected List<Identifier> biomes = new ArrayList<>();
     protected Decorations decorations = Decorations.DEFAULT;
     protected List<Identifier> effects = new ArrayList<>();
+    protected Loop loop = Loop.ZERO;
     /**
      * Stores identifiers of <b>worlds</b>, not dimension types.
      */
@@ -94,6 +95,7 @@ public abstract class AbstractSkybox {
         this.zRanges = conditions.getZRanges();
         this.xRanges = conditions.getXRanges();
         this.decorations = decorations;
+        this.loop = conditions.getLoop();
     }
 
     /**
@@ -159,7 +161,7 @@ public abstract class AbstractSkybox {
             }
 
             maxPossibleAlpha *= maxAlpha;
-            if (checkBiomes() && checkXRanges() && checkYRanges() && checkZRanges() && checkWeather() && checkEffect()) { // check if environment is invalid
+            if (checkBiomes() && checkXRanges() && checkYRanges() && checkZRanges() && checkWeather() && checkEffect() && checkLoop()) { // check if environment is invalid
                 if (alpha >= maxPossibleAlpha) {
                     alpha = maxPossibleAlpha;
                 } else {
@@ -175,7 +177,7 @@ public abstract class AbstractSkybox {
                 }
             }
         } else {
-            if (checkBiomes() && checkXRanges() && checkYRanges() && checkZRanges() && checkWeather() && checkEffect()) { // check if environment is invalid
+            if (checkBiomes() && checkXRanges() && checkYRanges() && checkZRanges() && checkWeather() && checkEffect() && checkLoop()) { // check if environment is invalid
                 alpha = 1f;
             } else {
                 alpha = 0f;
@@ -249,7 +251,7 @@ public abstract class AbstractSkybox {
      */
     protected boolean checkXRanges() {
         double playerX = Objects.requireNonNull(MinecraftClient.getInstance().player).getX();
-        return checkCoordRanges(playerX, this.xRanges);
+        return checkRanges(playerX, this.xRanges);
     }
 
     /**
@@ -257,7 +259,7 @@ public abstract class AbstractSkybox {
      */
     protected boolean checkYRanges() {
         double playerY = Objects.requireNonNull(MinecraftClient.getInstance().player).getY();
-        return checkCoordRanges(playerY, this.yRanges);
+        return checkRanges(playerY, this.yRanges);
     }
 
     /**
@@ -265,16 +267,33 @@ public abstract class AbstractSkybox {
      */
     protected boolean checkZRanges() {
         double playerZ = Objects.requireNonNull(MinecraftClient.getInstance().player).getZ();
-        return checkCoordRanges(playerZ, this.zRanges);
+        return checkRanges(playerZ, this.zRanges);
     }
 
     /**
-     * @return Whether the coordValue is within any of the minMaxEntries.
+     * @return Whether the current loop is valid for this skybox.
      */
-    private static boolean checkCoordRanges(double coordValue, List<MinMaxEntry> minMaxEntries) {
+    protected boolean checkLoop() {
+        if (!this.loop.getRanges().isEmpty() && this.loop.getDays() > 0) {
+            double currentTime = Objects.requireNonNull(MinecraftClient.getInstance().world).getTimeOfDay() - this.fade.getStartFadeIn();
+            while (currentTime < 0) {
+                currentTime += 24000 * this.loop.getDays();
+            }
+
+            double currentDay = (currentTime / 24000D) % this.loop.getDays();
+
+            return checkRanges(currentDay, this.loop.getRanges());
+        }
+        return true;
+    }
+
+    /**
+     * @return Whether the value is within any of the minMaxEntries.
+     */
+    private static boolean checkRanges(double value, List<MinMaxEntry> minMaxEntries) {
         return minMaxEntries.isEmpty() || minMaxEntries.stream()
-                .anyMatch(minMaxEntry -> Range.closedOpen(minMaxEntry.getMin(), minMaxEntry.getMax())
-                        .contains((float) coordValue));
+                .anyMatch(minMaxEntry -> Range.closed(minMaxEntry.getMin(), minMaxEntry.getMax())
+                        .contains((float) value));
     }
 
     /**
@@ -443,5 +462,9 @@ public abstract class AbstractSkybox {
 
     public List<MinMaxEntry> getZRanges() {
         return this.zRanges;
+    }
+
+    public Loop getLoop() {
+        return loop;
     }
 }
