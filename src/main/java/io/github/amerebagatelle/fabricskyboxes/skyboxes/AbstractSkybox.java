c360b21bbb90f035ad2d9a0f8f83aca9f2ae614d
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2022-12-08 06:21:44 +0800

    Add dimension identifier condition
    Currently the `worlds` list in conditions is for sky effects this means skies will still render in custom dimensions using the overworld sky effects preset. 
    Allows users to specifically target a dimension identifier.

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/skyboxes/AbstractSkybox.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/skyboxes/AbstractSkybox.java
@@ -162,7 +162,7 @@ public abstract class AbstractSkybox implements FSBSkybox {
         MinecraftClient client = MinecraftClient.getInstance();
         Objects.requireNonNull(client.world);
         Objects.requireNonNull(client.player);
-        if (this.conditions.getWorlds().isEmpty() || this.conditions.getWorlds().contains(client.world.getDimension().effects())) {
+        if ((this.conditions.getWorlds().isEmpty() || this.conditions.getWorlds().contains(client.world.getDimension().effects())) && (this.conditions.getDimensions().isEmpty() || this.conditions.getDimensions().contains(client.world.getRegistryKey().getValue()))) {
             return this.conditions.getBiomes().isEmpty() || this.conditions.getBiomes().contains(client.world.getRegistryManager().get(RegistryKeys.BIOME).getId(client.world.getBiome(client.player.getBlockPos()).value()));
         }
         return false;
