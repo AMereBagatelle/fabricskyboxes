684e386657424aa4f6784c56b120643b9ff07767
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-05-30 17:01:19 +0800

    Make fade times optional
    This commit makes fade times optional, if they are not specified nothing will be rendered; this means it disables the sky by default.

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Fade.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/util/object/Fade.java
@@ -4,12 +4,12 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public class Fade {
-    public static final Fade DEFAULT = new Fade(0, 0, 0, 0, false);
+    public static final Fade DEFAULT = new Fade(-1, -1, -1, -1, false);
     public static final Codec<Fade> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.INT.fieldOf("startFadeIn").forGetter(Fade::getStartFadeIn),
-            Codec.INT.fieldOf("endFadeIn").forGetter(Fade::getEndFadeIn),
-            Codec.INT.fieldOf("startFadeOut").forGetter(Fade::getStartFadeOut),
-            Codec.INT.fieldOf("endFadeOut").forGetter(Fade::getEndFadeOut),
+            Codec.INT.optionalFieldOf("startFadeIn", -1).forGetter(Fade::getStartFadeIn),
+            Codec.INT.optionalFieldOf("endFadeIn", -1).forGetter(Fade::getEndFadeIn),
+            Codec.INT.optionalFieldOf("startFadeOut", -1).forGetter(Fade::getStartFadeOut),
+            Codec.INT.optionalFieldOf("endFadeOut", -1).forGetter(Fade::getEndFadeOut),
             Codec.BOOL.optionalFieldOf("alwaysOn", false).forGetter(Fade::isAlwaysOn)
     ).apply(instance, Fade::new));
     private final int startFadeIn;
