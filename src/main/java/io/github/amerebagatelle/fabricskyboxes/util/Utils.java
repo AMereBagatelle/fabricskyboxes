c57b966f8f5b32800f061dd42b1a1ed635738ddd
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-07-12 23:57:28 +0800

    Improve alpha calculation & Alpha blend fog colors (#92)
    * Initial experimental alpha calculation
    
    * Normalize fade times
    
    * Clamp condition alpha
    
    * Fix fog color mixing
    
    * Add rain check by biome condition
    
    * Add configuration
    
    Allows user configurable time change transition duration. Setting the to 0 will never transition, values should be from 1 to maximum integer value.
    
    * new: Alpha blending for fog colors

--- src/main/java/io/github/amerebagatelle/fabricskyboxes/util/Utils.java
+++ src/main/java/io/github/amerebagatelle/fabricskyboxes/util/Utils.java
@@ -35,6 +35,14 @@ public class Utils {
         return result >= 0 ? result : result + 24000;
     }
 
+    /**
+     * Checks whether current time is within start and end time, this method also supports roll over checks
+     *
+     * @param currentTime The current world time
+     * @param startTime   The start time
+     * @param endTime     The end time
+     * @return Whether current time is within start and end time
+     */
     public static boolean isInTimeInterval(int currentTime, int startTime, int endTime) {
         if (currentTime < 0 || currentTime >= 24000) {
             throw new RuntimeException("Invalid current time, value must be between 0-23999: " + currentTime);
@@ -46,28 +54,60 @@ public class Utils {
         }
     }
 
-    public static float normalizeTime(float maxAlpha, int currentTime, int startTime, int endTime) {
-        if (!isInTimeInterval(currentTime, startTime, endTime))
-            return 0f;
+    /**
+     * Blends all fog colors using the alpha blending formula: (source * source_alpha) + (destination * (1 - source_alpha)).
+     *
+     * @param skyboxList      List of skyboxes to blend the fog colors from.
+     * @param initialFogColor The initial fog color to be blended with the skybox fog colors.
+     * @return The final blended fog color.
+     */
+    public static RGBA alphaBlendFogColors(List<Skybox> skyboxList, RGBA initialFogColor) {
+        List<RGBA> activeColors = skyboxList.stream()
+                .filter(Skybox::isActive) // check if active
+                .filter(FSBSkybox.class::isInstance) // check if our own skybox impl
+                .map(FSBSkybox.class::cast) // cast to our own skybox impl
+                .filter(fsbSkybox -> fsbSkybox.getProperties().isChangeFog())// check if fog is changed
+                .map(fsbSkybox -> new RGBA(fsbSkybox.getProperties().getFogColors().getRed(),
+                        fsbSkybox.getProperties().getFogColors().getGreen(),
+                        fsbSkybox.getProperties().getFogColors().getBlue(),
+                        fsbSkybox.getAlpha() / fsbSkybox.getProperties().getMaxAlpha()))
+                .collect(Collectors.toList()); // map RGB fog colors and A to skybox alpha
+        if (activeColors.size() == 0) {
+            return null;
+        } else {
+            RGBA destination = initialFogColor;
+            for (RGBA source : activeColors) {
+                // Alpha blending
+                float sourceAlphaInv = 1f - source.getAlpha();
 
-        int range = (endTime - startTime + 24000) % 24000;
-        if (range == 0) {
-            return 0f;
+                float red = (source.getRed() * source.getAlpha()) + (destination.getRed() * sourceAlphaInv);
+                float green = (source.getGreen() * source.getAlpha()) + (destination.getGreen() * sourceAlphaInv);
+                float blue = (source.getBlue() * source.getAlpha()) + (destination.getBlue() * sourceAlphaInv);
+                float alpha = (source.getAlpha() * source.getAlpha()) + (destination.getAlpha() * sourceAlphaInv);
+
+                destination = new RGBA(red, green, blue, alpha);
+            }
+            return destination;
         }
-
-        float position = (float) ((currentTime - startTime + 24000) % 24000) / range;
-        return position * maxAlpha;
     }
 
-    public static RGBA blendFogColorsFromSkies(List<Skybox> skyboxList, RGBA originalFogColor) {
+    /**
+     * Uses weighted additive color mixing and then applies the alpha blending formula: (source * source_alpha) + (destination * (1 - source_alpha)) with the initial fog color.
+     *
+     * @param skyboxList      List of skyboxes to blend the fog colors from.
+     * @param initialFogColor The initial fog color to be blended with the skybox fog colors.
+     * @return The weighted additive color with the final blended color using the alpha blending formula along with the initial fog color.
+     */
+    public static RGBA weightedAdditiveBlendFogColors(List<Skybox> skyboxList, RGBA initialFogColor) {
         float[] colorSum = new float[4];
         List<RGBA> activeColors = skyboxList.stream()
+                .filter(Skybox::isActive)
                 .filter(FSBSkybox.class::isInstance)
                 .map(FSBSkybox.class::cast)
                 .filter(fsbSkybox -> fsbSkybox.getProperties().isChangeFog())
                 .map(fsbSkybox -> new RGBA(fsbSkybox.getProperties().getFogColors().getRed(),
-                    fsbSkybox.getProperties().getFogColors().getGreen(),
-                    fsbSkybox.getProperties().getFogColors().getBlue(),
+                        fsbSkybox.getProperties().getFogColors().getGreen(),
+                        fsbSkybox.getProperties().getFogColors().getBlue(),
                         fsbSkybox.getAlpha() / fsbSkybox.getProperties().getMaxAlpha()))
                 .collect(Collectors.toList());
         if (activeColors.size() == 0) {
@@ -77,19 +117,65 @@ public class Utils {
             colorSum[0] += rgba.getRed() * rgba.getAlpha();
             colorSum[1] += rgba.getGreen() * rgba.getAlpha();
             colorSum[2] += rgba.getBlue() * rgba.getAlpha();
-            colorSum[3] += rgba.getAlpha();
+            colorSum[3] += rgba.getAlpha(); // this should never be zero.
         }
-        float finalAlpha = colorSum[3] / activeColors.size();
+        float finalAlpha = colorSum[3];
         final RGBA activeColorsMixed = new RGBA(colorSum[0] / finalAlpha, colorSum[1] / finalAlpha, colorSum[2] / finalAlpha);
 
         Optional<RGBA> activeColorsHighestAlpha = activeColors.stream().max(Comparator.comparingDouble(RGBA::getAlpha));
         float activeColorsMaxAlpha = activeColorsHighestAlpha.get().getAlpha();
 
         float diffMul = 1f - activeColorsMaxAlpha;
-        final RGBA originalFogColorModified = new RGBA(originalFogColor.getRed() * diffMul, originalFogColor.getGreen() * diffMul, originalFogColor.getBlue() * diffMul);
+        final RGBA originalFogColorModified = new RGBA(initialFogColor.getRed() * diffMul, initialFogColor.getGreen() * diffMul, initialFogColor.getBlue() * diffMul);
         final RGBA activeColorsMixedFinal = new RGBA(activeColorsMixed.getRed() * activeColorsMaxAlpha, activeColorsMixed.getGreen() * activeColorsMaxAlpha, activeColorsMixed.getBlue() * activeColorsMaxAlpha);
 
         return new RGBA(originalFogColorModified.getRed() + activeColorsMixedFinal.getRed(), originalFogColorModified.getGreen() + activeColorsMixedFinal.getGreen(), originalFogColorModified.getBlue() + activeColorsMixedFinal.getBlue());
+    }
+
+    /**
+     * Calculates the fade alpha
+     *
+     * @param maxAlpha     The maximum alpha value
+     * @param currentTime  The current world time
+     * @param startFadeIn  The fade in start time
+     * @param endFadeIn    The fade in end time
+     * @param startFadeOut The fade out start time
+     * @param endFadeOut   The fade out end time
+     * @return Fade Alpha
+     */
+    public static float calculateFadeAlphaValue(float maxAlpha, int currentTime, int startFadeIn, int endFadeIn, int startFadeOut, int endFadeOut) {
+        if (isInTimeInterval(currentTime, endFadeIn, startFadeOut)) {
+            return maxAlpha;
+        } else if (isInTimeInterval(currentTime, startFadeIn, endFadeIn)) {
+            return ((float) (currentTime - startFadeIn) / (endFadeIn - startFadeIn)) * maxAlpha;
+        } else if (isInTimeInterval(currentTime, startFadeOut, endFadeOut)) {
+            return 1.0f - ((float) (currentTime - startFadeOut) / (endFadeOut - startFadeOut)) * maxAlpha;
+        } else {
+            return 0f;
+        }
+    }
+
+    /**
+     * Calculates the condition alpha
+     *
+     * @param maxAlpha  The maximum alpha value
+     * @param lastAlpha The last condition alpha value
+     * @param duration  The duration
+     * @param in        Whether it will transition in or out
+     * @return condition alpha
+     */
+    public static float calculateConditionAlphaValue(float maxAlpha, float lastAlpha, int duration, boolean in) {
+        if (duration == 0) {
+            return lastAlpha;
+        } else if (in && maxAlpha == lastAlpha) {
+            return maxAlpha;
+        } else if (!in && lastAlpha == 0f) {
+            return 0f;
+        } else {
+            float alphaChange = maxAlpha / duration;
+            float result = in ? lastAlpha + alphaChange : lastAlpha - alphaChange;
+            return MathHelper.clamp(result, 0f, maxAlpha);
+        }
     }
 
     public static Codec<Integer> getClampedInteger(int min, int max) {
